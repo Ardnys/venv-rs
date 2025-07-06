0541eef1ed6cd7b64952d38c51746bbb83b31877
Ardnys <semresavas35@gmail.com>
2025-07-06 08:23:25 +0300

    add more commands and flags

--- src/commands.rs
+++ src/commands.rs
@@ -5,8 +5,13 @@ use clap::{Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None)]
 pub struct Cli {
+    /// Directory containing virtual environments
     #[arg(short, long, value_name = "DIR")]
     pub venvs_dir: Option<PathBuf>,
+
+    /// Exit with output (for using with shell commands)
+    #[arg(short)]
+    pub output: bool,
 
     #[command(subcommand)]
     pub kind: Option<Kind>,
@@ -14,7 +19,11 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Kind {
+    /// Inspect a single virtual environment
     Venv {
+        /// Path to virtual environment
+        path: PathBuf,
+        /// Show package sizes
         #[arg(short, long)]
         show_size: bool,
     },
